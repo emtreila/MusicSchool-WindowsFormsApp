85211cadceed69b554fba276c5fd23841cbb2364
emtreila <bmelania214@gmail.com>
2026-04-14 19:44:17 +0300

    main form & database connectivity

---
+++ MusicSchoolFormsApp/WindowsFormsAppLAB1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsAppLAB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvT1 = new System.Windows.Forms.DataGridView();
+            this.dgvT2 = new System.Windows.Forms.DataGridView();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvT1
+            // 
+            this.dgvT1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvT1.Location = new System.Drawing.Point(29, 12);
+            this.dgvT1.Name = "dgvT1";
+            this.dgvT1.RowTemplate.Height = 24;
+            this.dgvT1.Size = new System.Drawing.Size(623, 222);
+            this.dgvT1.TabIndex = 0;
+            // 
+            // dgvT2
+            // 
+            this.dgvT2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvT2.Location = new System.Drawing.Point(29, 258);
+            this.dgvT2.Name = "dgvT2";
+            this.dgvT2.RowTemplate.Height = 24;
+            this.dgvT2.Size = new System.Drawing.Size(623, 223);
+            this.dgvT2.TabIndex = 1;
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(693, 225);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(85, 41);
+            this.btnSaveData.TabIndex = 2;
+            this.btnSaveData.Text = "Save data";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 506);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.dgvT2);
+            this.Controls.Add(this.dgvT1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvT2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvT1;
+        private System.Windows.Forms.DataGridView dgvT2;
+        private System.Windows.Forms.Button btnSaveData;
+    }
+}
+
---
+++ MusicSchoolFormsApp/WindowsFormsAppLAB1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppLAB1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection dbConn;
+        SqlDataAdapter daStudents, daGrades;
+        SqlCommandBuilder cb;
+        DataSet ds;
+        BindingSource bsStudents, bsGrades;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            daGrades.Update(ds, "Grades");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConn = new SqlConnection("Server = EMTREILA-PC\\SQLEXPRESS ; dATABASE = MusicSchool; Integrated Security = true");
+
+            ds = new DataSet();
+
+            daStudents = new SqlDataAdapter("SELECT * FROM Students", dbConn);
+            daGrades = new SqlDataAdapter("SELECT * FROM Grades", dbConn);
+            cb = new SqlCommandBuilder(daGrades);
+
+            daStudents.Fill(ds, "Students");
+            daGrades.Fill(ds, "Grades");
+
+            DataRelation dr = new DataRelation("FK_Grades_Students",
+                                                ds.Tables["Students"].Columns["StudentID"],
+                                                ds.Tables["Grades"].Columns["StudentID"]);
+            ds.Relations.Add(dr);
+
+            bsStudents = new BindingSource();
+            bsStudents.DataSource = ds;
+            bsStudents.DataMember = "Students";
+
+            bsGrades = new BindingSource();
+            bsGrades.DataSource = bsStudents;
+            bsGrades.DataMember = "FK_Grades_Students";
+
+            dgvT1.DataSource = bsStudents;
+            dgvT2.DataSource = bsGrades;
+        }
+    }
+
+
+}
